214989b33850b1b893bdec1e9c11062c5eebdb39
kennethchuson <kennethchuson@yahoo.com>
2022-02-11 16:49:04 -0800

    added final result for fthe HA#3

--- HA#3/main.py
+++ HA#3/main.py
@@ -46,6 +46,13 @@ nltk.download('gutenberg')
 class Next_word_predictor_using_a_bigram_language_model(object):
 
     def __init__(self, user_input, what_textFile):
+        '''
+            user_input - takes the user input required.
+            what_textFile - what the textfile is.
+            get_list_gutenberg_text - this will give the list of words from the textfile from the gutenberg corpus nltk library.
+            store_text - store the list of texts.
+            bigram_text - store the list of bigram text. 
+        '''
         self.user_input = user_input
         self.what_textFile = what_textFile
         self.get_list_gutenberg_text = nltk.corpus.gutenberg.words(what_textFile)
@@ -54,6 +61,12 @@ class Next_word_predictor_using_a_bigram_language_model(object):
 
 
     def print_words_from_nltk_corups(self):
+        '''
+            starting_word_number - is set to 0. 
+            ending_word_number - is set to 1000 words for now.
+
+            self.store_text.append(word) - this will store each of the text from the corpus. 
+        '''
         starting_word_number = 0
         ending_word_number = 1000
 
@@ -63,16 +76,37 @@ class Next_word_predictor_using_a_bigram_language_model(object):
             print(word, sep='', end='\n')
 
     def print_bigrams_words(self):
-        
+        '''
+            bigram_text - get the bigram text and this will get the list of tuples in an array.
+
+            [(word1, word2), (word2, word3), (word3, word4) .... (word(n - 1), word(n))] 
+    
+        '''
         self.bigram_text = list(nltk.bigrams(self.store_text))
 
     def probability_bigrams_words(self):
         pass
         '''
-          w1 = frequency for w1
-          w2 = frequency for w2
-        
-          prob = (count(w1, w2)/count(w1))
+        Goal: 
+              w1 = frequency for w1
+              w2 = frequency for w2
+            
+              prob = (count(w1, w2)/count(w1))
+
+        sentence_text - this will convert words in an array from the store_text.
+
+        store_count_w1 - keep on track how many words in them.
+                    given_sentence = "I will go to California to meet my friend"
+                    For example: ["I", "will", "go", "to", "California", "to", "meet", "my", "friend"]
+                        this will convert to [1, 1, 1, 2, 1, 2, 1, 1, 1] for the each word frequencies count.
+
+        map_bigrams_words - This will map the list of tuples bigram words
+                                                    (word1, word2)                frequencies as number (n) 
+                                map_bigrams_words[<Where the tuples as keys>] = <then the frequencies>
+        result - store the result as dictionary of map_bigrams_words after computing the probability (prob).
+
+        result_map - this will help to print out the result. 
+         
         '''
         sentence_text = ' '.join(word for word in self.store_text)
         
@@ -96,7 +130,14 @@ class Next_word_predictor_using_a_bigram_language_model(object):
             if self.user_input in key:
                 result_map[key] = calculate
 
-        K = 2
+        '''
+            K - how many times do you want to see the closer probability from the user input.
+
+            result - using operator python library to help determine sorting dictionary.
+            
+        '''
+
+        K = 3
 
         result = dict(sorted(result_map.items(), key = itemgetter(1))[:K])
 
@@ -106,7 +147,20 @@ class Next_word_predictor_using_a_bigram_language_model(object):
     
         
     
+'''
+    user_input - takes the user input of the word.
+    what_textFile - determine what the text file is. For right now "shakespear-hamlet.txt" is the
+                    default file for now.
+    next_word_predict - call a class and this class has core required functions of this assignment.
 
+    print_words_from_nltk_corups - it will print the words from the nltk corpus.
+
+    print_bigrams_words - orint the words with the lists of bigram words.
+
+    probability_bigrams_words - call the compute probability of bigrams words and analyze from the user input. 
+
+
+'''
 
 user_input = str(input("Input a word: "))
 what_textFile = 'shakespeare-hamlet.txt'
