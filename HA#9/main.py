37dc8691e60c0455bbd41d2c5baf6cea1d54fd4c
kennethchuson <kennethchuson@yahoo.com>
2022-03-29 14:36:05 -0700

    added HA#9 but not finish

---
+++ HA#9/main.py
@@ -0,0 +1,15 @@
+
+from main2 import addition 
+from main2 import Test
+
+
+out = addition(2,5)
+
+a = Test() 
+a.set(10000) 
+
+print("class get: ", a.get())
+
+print(out) 
+
+
---
+++ HA#9/main2.py
@@ -0,0 +1,18 @@
+
+
+
+class Test: 
+    def __init__(self): 
+        self.x = 0 
+    def set(self, x_input): 
+        self.x = x_input
+    
+    def get(self): 
+        return self.x 
+
+
+
+
+def addition(a, b): 
+    return a + b 
+
